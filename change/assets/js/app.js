fda4d14907ad2217824c5c5d0eed2e463f8bd759
MahammadMustafayev <tu7ekdm78@code.edu.az>
2022-12-14 11:01:20 +0400

    added change image

---
+++ change/assets/js/app.js
@@ -0,0 +1,31 @@
+
+let proImg=document.querySelector(".pro-img");
+function changeImage(event) {
+    proImg.src=event.children[0].src;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function changeImage(event) {
+//     proImg.src=event.children[0].src
+//     for (let i = 0; i < thumbs.length; ++i) {
+//         thumbs[i].classList.remove("active");
+//     }
+//     event.classList.add("active");
+// }
+
